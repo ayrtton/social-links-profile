f115f03816b725e54d48be8634541a1ef116c2d7
Ayrton <ayrtton.vinicius@gmail.com>
2024-02-27 17:48:31 -0300

    Create the mixins for font sizes and flexboxes

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,17 +1,22 @@
+import profileImage from '../assets/images/avatar-jessica.jpeg';
+
 function Card() {
     return (
         <main>
-            <div id="profile-info">
-                <h1>Jessica Randall</h1>
-                <span>London, United Kingdom</span>
-                <p>{'"'}Front-end developer and avid reader.{'"'}</p>
-            </div>
-            <div id="links">
-                <div className="social-link">GitHub</div>
-                <div className="social-link">Frontend Mentor</div>
-                <div className="social-link">LinkedIn</div>
-                <div className="social-link">Twitter</div>
-                <div className="social-link">Instagram</div>
+            <div id="card">
+                <div id="profile-info">
+                    <img src={profileImage} alt="" srcSet="" />
+                    <h1>Jessica Randall</h1>
+                    <span>London, United Kingdom</span>
+                    <p>{'"'}Front-end developer and avid reader.{'"'}</p>
+                </div>
+                <div id="links">
+                    <div className="social-link">GitHub</div>
+                    <div className="social-link">Frontend Mentor</div>
+                    <div className="social-link">LinkedIn</div>
+                    <div className="social-link">Twitter</div>
+                    <div className="social-link">Instagram</div>
+                </div>
             </div>
         </main>
     );
